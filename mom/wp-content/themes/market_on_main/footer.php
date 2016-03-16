fbc7f103b2234d897911621394d8db5274765146
Missy <mzswope98@yahoo.com>
2016-03-16 14:47:57 +0100

    Changed picture shape, border margins and added content to Vendor page for static site.  Added header to WP site.

--- mom/wp-content/themes/market_on_main/footer.php
+++ mom/wp-content/themes/market_on_main/footer.php
@@ -11,18 +11,62 @@
 
 ?>
 
-	</div><!-- #content -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-4">
+            <nav class="navbar nav-pills nav-stacked">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span>Menu</span>
+                  </button>
+                </div><!-- navbar-header -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'market_on_main' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'market_on_main' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'market_on_main' ), 'market_on_main', '<a href="http://underscores.me/" rel="designer">RumpleTwins</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+                <div class="collapse navbar-collapse" id="navbar-collapse-2">
+                  <ul class="nav navbar-nav">
+                    <li><a href="mom.html">Home</a></li>
+                    <li><a href="mom_calendar.html">Calendar</a></li>
+                    <li><a href="mom_vendors.html">Vendors</a></li>
+                    <li><a href="mom_our_staff.html">Our Staff</a></li>
+                    <li class="active"><a href="mom_about_us">About Us <span class="sr-only">(current)</span></a></li>
+                  </ul>
+                  
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+          </div><!-- col-4 -->
+          <div class="col-md-6 col-sm-8 text-center" id="footer_hours">
+            <div>Tuesday - Friday 11:00am - 7:00pm</div>
+            <div>Saturday 8:00am - 4:00pm</div><br>
+            <ul>
+              <div>Follow Us:&nbsp;</div>
+              <li><a href="" alt="facebook link"></a>F&nbsp;</li>
+              <li><a href="" alt="instagram link"></a>C</li>
+            </ul>
+          </div><!-- col-4 -->
+          <div class="col-md-3 col-sm-12 text-right">
+            <address>
+              Market on Main<br>
+              331 E. Main St<br>
+              Ottumwa, IA 52501<br>
+              641-682-0261
+            </address>
+          </div><!-- col-4 -->
+        </div><!-- row -->
+      </div><!-- container -->
+    </footer>
+
 
 <?php wp_footer(); ?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+  <script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
 
 </body>
 </html>
--- mom/wp-content/themes/market_on_main/header.php
+++ mom/wp-content/themes/market_on_main/header.php
@@ -9,18 +9,38 @@
  * @package Market_on_Main
  */
 
-?><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--Bootstrap core CSS -->
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css">
+  <!--Font Awesome icons -->
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css">
+
+<!--Avenir Fonts -->
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/fonts/AvenirLTStd-Book.otf">
+
+  <!--Google Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
+
 
 <?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+
+  <!--[if lt IE 9]>
+  <![endif]-->
+  
 </head>
 
 <body <?php body_class(); ?>>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'market_on_main' ); ?></a>
 
@@ -28,15 +48,17 @@
 
 <!-- Header Image and logo -->
   <div class="container">
-    <img src="#" alt="Market on Main logo">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/momLogo.png" alt="Market on Main logo">
 
   </div><!-- container -->
+
+</header>
 
 <!-- Navigation -->
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
           <span class="sr-only">Toggle navigation</span>
           <span>Menu</span>
         </button>
@@ -50,11 +72,10 @@
 
 				'theme_location' 	=> 'primary',
 				'container'			=> 'nav',
-				'container_class'	=> 'nav navbar-nav navbar-right',
-				'menu_class'		=> 'nav navbar-nav navbar-right'	
+				'container_class'	=> 'nav navbar-nav navbar-left',
+				'menu_class'		=> 'nav navbar-nav navbar-left'	
 				));
 		?>
 
     </div><!-- /.container-fluid -->
   </nav>
-</header>
